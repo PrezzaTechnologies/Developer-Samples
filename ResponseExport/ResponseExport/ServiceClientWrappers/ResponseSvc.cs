a25d0c2c4b26f1f00119045db80568269a552f88
Kevin Fisher <Kfisher@checkbox.com>
2012-11-14 14:40:10 -0500

    Response Export Sample and Getting Started Guide

---
+++ ResponseExport/ResponseExport/ServiceClientWrappers/ResponseSvc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Checkbox.Wcf.Services.Proxies;
+using ResponseExport.ResponseDataService;
+
+namespace ResponseExport.ServiceClientWrappers
+{
+    class ResponseSvc
+    {
+        private ResponseDataServiceClient Client { get; set; }
+
+        public ResponseSvc()
+        {
+            Client = new ResponseDataServiceClient("BasicHttpBinding_IResponseDataService");
+        }
+
+        public Guid? GetResponseGuidForUser(string authTicket, int surveyId, string userName)
+        {
+            var page =
+                Client.ListSurveyResponses(authTicket, surveyId, 0, 1, "UniqueIdentifier", userName, "", true).
+                    GetResults();
+
+            var firstOrDefault = page.ResultPage.FirstOrDefault();
+            return firstOrDefault != null ? (Guid?)firstOrDefault.Guid : null;
+        }
+
+        public ResponseItemAnswerData[] GetAnswersForResponseByGuid(string authTicket, int surveyId, string languageCode, Guid responseGuid)
+        {
+            return Client.GetAnswersForResponseByGuid(authTicket, surveyId, languageCode, responseGuid).GetResults();
+        }
+    }
+}
---
+++ ResponseExport/ResponseExport/ServiceClientWrappers/SurveyMgmtSvc.cs
@@ -0,0 +1,20 @@
+﻿using ResponseExport.SurveyManagementService;
+
+namespace ResponseExport.ServiceClientWrappers
+{
+    class SurveyMgmtSvc
+    {
+        private static SurveyManagementServiceClient Client { get; set; }
+
+        public SurveyMgmtSvc()
+        {
+            Client = new SurveyManagementServiceClient("BasicHttpBinding_ISurveyManagementService");
+        }
+
+        public int? GetSurveyIdByName(string authTicket, string surveyName)
+        {
+            var surveyInfo = Client.GetSurveyInfoByName(authTicket, surveyName).GetResults();
+            return surveyInfo.Id;
+        }
+    }
+}
