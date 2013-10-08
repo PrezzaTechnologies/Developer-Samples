96f90e7bcae02dce6f6d5f3006e3ee71608cc3ca
Petr Lysachev <petr.lysachev@dataart.com>
2013-10-08 19:37:37 +0400

    ListAvailableSurveys Sample

---
+++ ListAvailableSurveys/ListAvailableSurveys/make_proxies.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\wsdl.exe"  /namespace:ListAvailableSurveys /sharetypes /l:cs /protocol:SOAP /o:ListAvailableSurveys\CheckboxWebServices.cs [CHECKBOX_ROOT]/Services/SurveyManagementService.svc?singleWsdl [CHECKBOX_ROOT]/Services/AuthenticationService.svc?singleWsdl
+
