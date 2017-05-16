ecaecc09d332965072d30282c7402fb7450abe92
Kevin Fisher <kfisher@checkbox.com>
2017-05-16 11:09:13 -0700

    Windows Console sample added for Response Management. Sample pulls response data from Checkbox then saves it to a local database.

---
+++ .NET Samples/ResponseExport-Console/SampleDatabase/ResponseData.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[ResponseData]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ResponseId] INT NOT NULL, 
+    [ResponseGuid] NVARCHAR(50) NULL, 
+    [Started] DATETIME NULL, 
+    [Ended] DATETIME NULL, 
+    [TotalTime] NVARCHAR(50) NULL, 
+    [LastEdit] DATETIME NULL, 
+    [IP] NVARCHAR(50) NULL, 
+    [Language] NVARCHAR(50) NULL, 
+    [UniqueIdentifier] NVARCHAR(50) NULL, 
+    [Invitee] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Item1526] NVARCHAR(50) NULL, 
+    [Item1527] NVARCHAR(MAX) NULL
+)
